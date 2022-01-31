5247f134f7d72983abe213db542746ae6eeaa1e3
Mark Honeychurch <mark@honeychurch.org>
2022-02-01 01:22:22 +1300

    Add service worker

---
+++ sw.js
@@ -0,0 +1,29 @@
+const files = [
+	'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css',
+	'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js',
+	'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.1/font/bootstrap-icons.css',
+	'https://unpkg.com/petite-vue?module',
+	'/basex.js',
+	'/index.html',
+];
+
+self.addEventListener('install', (event) => {
+	event.waitUntil(
+		caches.open('v1').then((cache) => {
+			return cache.addAll(files);
+		})
+	);
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((resp) => {
+      return resp || fetch(event.request).then((response) => {
+        return caches.open('v1').then((cache) => {
+          cache.put(event.request, response.clone());
+          return response;
+        });
+      });
+    })
+  );
+});
